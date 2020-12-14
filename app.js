b8bc3b1071a45e1c18f7453b3236e0b4e3f5e774
jatin <jatinchourasia3@gmail.com>
2020-12-14 15:28:13 -0800

    added functionalities

--- app.js
+++ app.js
@@ -1,23 +1,100 @@
 class DrumKit {
   constructor() {
-    this.pad = document.querySelectorAll(".pad");
+    this.pads = document.querySelectorAll(".pad");
     this.kickAudio = document.querySelector(".kick-sound");
     this.snareAudio = document.querySelector(".snare-sound");
-    this.hihatAudio = document.querySelector(".hihaat-sound");
+    this.hihatAudio = document.querySelector(".hihat-sound");
     this.index = 0;
+    this.bpm = 150; // speed
+    this.playBtn = document.querySelector(".play");
+    this.isPlaying = null;
+    this.currentKick = "./sounds/kick-classic.wav";
+    this.currentSnare = "./sounds/snare-acoustic01.wav";
+    this.currentHihat = "./sounds/hihat-acoustic01.wav";
+    this.selects = document.querySelectorAll("select");
+  }
+
+  changeSound(e) {
+    const selectionName = e.target.name;
+    const selectionValue = e.target.value;
+    switch (selectionName) {
+      case "kick-select":
+        this.kickAudio.src = selectionValue;
+        break;
+      case "snare-select":
+        this.snareAudio.src = selectionValue;
+        break;
+      case "hihat-select":
+        this.hihatAudio.src = selectionValue;
+        break;
+    }
+  }
+
+  activePad() {
+    this.classList.toggle("active");
   }
 
   repeat() {
     let step = this.index % 8;
     const activeBars = document.querySelectorAll(`.b${step}`);
-    console.log(activeBars);
+    //loop over the pads
+    activeBars.forEach((bar) => {
+      bar.style.animation = "playTrack 0.3s alternate ease-in 2";
+      if (bar.classList.contains("active")) {
+        // check each sounds
+        if (bar.classList.contains("kick-pad")) {
+          this.kickAudio.currentTime = 0;
+          this.kickAudio.play();
+        }
+        if (bar.classList.contains("snare-pad")) {
+          this.snareAudio.currentTime = 0;
+
+          this.snareAudio.play();
+        }
+        if (bar.classList.contains("hihat-pad")) {
+          this.hihatAudio.currentTime = 0;
+
+          this.hihatAudio.play();
+        }
+      }
+    });
     this.index++;
   }
   start() {
-    setInterval(() => {
-      this.repeat();
-    }, 1000);
+    const interval = (60 / this.bpm) * 1000;
+
+    // check is playing
+    if (!this.isPlaying) {
+      this.isPlaying = setInterval(() => {
+        this.repeat();
+      }, interval);
+      this.playBtn.innerHTML = "Pause";
+    } else {
+      clearInterval(this.isPlaying);
+      this.playBtn.innerHTML = "Play";
+
+      this.isPlaying = null;
+    }
   }
 }
 
+// Event litseners
+
 const drumKit = new DrumKit();
+
+drumKit.selects.forEach((select) => {
+  select.addEventListener("change", function (e) {
+    drumKit.changeSound(e);
+  });
+});
+
+drumKit.pads.forEach((pad) => {
+  pad.addEventListener("click", drumKit.activePad);
+  pad.addEventListener("animationend", function () {
+    this.style.animation = "";
+  });
+});
+
+drumKit.playBtn.addEventListener("click", () => {
+  drumKit.start();
+});
